2a37e3606e2af1f6d67fb8cbaa78f71dcf2320af
Alex <69731857+a1exkarpenko@users.noreply.github.com>
2022-05-31 17:15:37 +0300

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
+        publicPath: '/',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
